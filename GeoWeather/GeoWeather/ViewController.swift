1d7e0bc41682173b63d2223909f1a260ad5aac26
jscodebit <soujanya.janapatla@gmail.com>
2020-11-11 22:58:55 -0600

    Added project skeleton and background images

---
+++ GeoWeather/GeoWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoWeather
+//
+//  Created by student on 11/11/20.
+//  Copyright © 2020 NorthWest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
